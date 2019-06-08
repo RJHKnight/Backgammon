d0ed9022306ac040efbd8482b78fd11ac5f43035
rjk <rjhknight@gmail.com>
2019-06-08 17:15:30 +1000

    Fixing test cases.

--- tests/testthat/testBoard.R
+++ tests/testthat/testBoard.R
@@ -1,6 +1,4 @@
 context("Basic test of the backgammon board")
-library(Backgammon)
-library(tidyverse)
 
 test_that("initial board state is valid", {
   expect_equal(TRUE, validate(getStartingBoard()))
@@ -11,12 +9,12 @@ test_that("boundary checks for board", {
   initialBoard <- getStartingBoard()
 
   # White boundary check
-  expect_equal(FALSE, canMove(initialBoard, thisColour = WHITE, point = 19, roll = 6))
-  expect_equal(TRUE,  canMove(initialBoard, thisColour = WHITE, point = 19, roll = 4))
+  expect_equal(FALSE, canMove(initialBoard, thisColour = WHITE, thisPoint = 19, roll = 6))
+  expect_equal(TRUE,  canMove(initialBoard, thisColour = WHITE, thisPoint = 19, roll = 4))
 
   # Red boundary check
-  expect_equal(FALSE, canMove(initialBoard, thisColour = RED, point = 6, roll = 6))
-  expect_equal(TRUE,  canMove(initialBoard, thisColour = RED, point = 6, roll = 4))
+  expect_equal(FALSE, canMove(initialBoard, thisColour = RED, thisPoint = 6, roll = 6))
+  expect_equal(TRUE,  canMove(initialBoard, thisColour = RED, thisPoint = 6, roll = 4))
 })
 
 test_that("valid can moves", {
@@ -24,20 +22,20 @@ test_that("valid can moves", {
   initialBoard <- getStartingBoard()
 
   # White
-  expect_equal(FALSE, canMove(initialBoard, thisColour = WHITE, point = 12, roll = 1))
-  expect_equal(TRUE, canMove(initialBoard, thisColour = WHITE, point = 12, roll = 2))
-  expect_equal(TRUE, canMove(initialBoard, thisColour = WHITE, point = 12, roll = 3))
-  expect_equal(TRUE, canMove(initialBoard, thisColour = WHITE, point = 12, roll = 4))
-  expect_equal(TRUE, canMove(initialBoard, thisColour = WHITE, point = 12, roll = 5))
-  expect_equal(TRUE, canMove(initialBoard, thisColour = WHITE, point = 12, roll = 6))
+  expect_equal(FALSE, canMove(initialBoard, thisColour = WHITE, thisPoint = 12, roll = 1))
+  expect_equal(TRUE, canMove(initialBoard, thisColour = WHITE, thisPoint = 12, roll = 2))
+  expect_equal(TRUE, canMove(initialBoard, thisColour = WHITE, thisPoint = 12, roll = 3))
+  expect_equal(TRUE, canMove(initialBoard, thisColour = WHITE, thisPoint = 12, roll = 4))
+  expect_equal(TRUE, canMove(initialBoard, thisColour = WHITE, thisPoint = 12, roll = 5))
+  expect_equal(TRUE, canMove(initialBoard, thisColour = WHITE, thisPoint = 12, roll = 6))
 
   # Red
-  expect_equal(FALSE, canMove(initialBoard, thisColour = RED, point = 13, roll = 1))
-  expect_equal(TRUE, canMove(initialBoard, thisColour = RED, point = 13, roll = 2))
-  expect_equal(TRUE, canMove(initialBoard, thisColour = RED, point = 13, roll = 3))
-  expect_equal(TRUE, canMove(initialBoard, thisColour = RED, point = 13, roll = 4))
-  expect_equal(TRUE, canMove(initialBoard, thisColour = RED, point = 13, roll = 5))
-  expect_equal(TRUE, canMove(initialBoard, thisColour = RED, point = 13, roll = 6))
+  expect_equal(FALSE, canMove(initialBoard, thisColour = RED, thisPoint = 13, roll = 1))
+  expect_equal(TRUE, canMove(initialBoard, thisColour = RED, thisPoint = 13, roll = 2))
+  expect_equal(TRUE, canMove(initialBoard, thisColour = RED, thisPoint = 13, roll = 3))
+  expect_equal(TRUE, canMove(initialBoard, thisColour = RED, thisPoint = 13, roll = 4))
+  expect_equal(TRUE, canMove(initialBoard, thisColour = RED, thisPoint = 13, roll = 5))
+  expect_equal(TRUE, canMove(initialBoard, thisColour = RED, thisPoint = 13, roll = 6))
 
 })
 
